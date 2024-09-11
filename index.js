430364b17f512a9a2989475efe60514b7cb21d9f
Haoroux <52127278+Haoroux@users.noreply.github.com>
2024-09-11 16:55:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
-const data = document.getElementById("question");
+const numData = document.getElementById("numInput");
+const baseMod = document.getElementById("baseModeleInput");
+
 const answer = document.getElementById("answer");
 const btn = document.getElementById("clicker");
 const convertedNum = [];
 
-console.log(data);
-console.log(answer);
-console.log(btn);
+// console.log(numData);
+// console.log(answer);
+// console.log(btn);
 
 btn.addEventListener("click", (event) => {
-    // btn.innerHTML = `Nombre : ${data.value}`;
-    numToConv = data.value;
-    decToMaya(numToConv)
+    // btn.innerHTML = `Nombre : ${numData.value}`;
+    if (baseMod.value > 1){
+        decToAsked(numData.value,baseMod.value);
+    } else{
+        alert("Vous devez mettre un chiffre supérieur a 1 en tant que base!")
+    }
     answer.innerHTML = convertedNum;
 });
 
-function decToBin(decNum){
+function decToAsked(decNum,baseModele){
     convertedNum.length = 0;
     while (decNum != 0){
-        quotient = Math.floor(decNum/2);
-        remainder = decNum % 2;
-        if (remainder > 0){
-            convertedNum.unshift(1);
-            console.log("add one");
-            decNum = quotient;
-            console.log(convertedNum)
-        } else{
-            convertedNum.unshift(0);
-            console.log("add zero");
-            decNum = quotient;
-            console.log(convertedNum)
-        }
-    }
-    return(convertedNum)
-}
-
-function decToMaya(decNum){
-    convertedNum.length = 0;
-    while (decNum != 0){
-        quotient = Math.floor(decNum/20);
-        remainder = decNum % 20;
+        quotient = Math.floor(decNum/baseModele);
+        remainder = decNum % baseModele;
         if (remainder > 0){
             convertedNum.unshift(remainder);
             console.log("add rem");
